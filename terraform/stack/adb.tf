0f4f72ca0f702d332fba2c41272178a62905f146
root <root@thinkpad-pc.localdomain>
2026-01-26 17:10:17 +0900

    update

--- terraform/stack/adb.tf
+++ terraform/stack/adb.tf
@@ -1,30 +1,29 @@
-
 resource "oci_database_autonomous_database" "generated_database_autonomous_database" {
-  admin_password                                 = var.adb_password
+  admin_password                                 = var.db_password
   autonomous_maintenance_schedule_type           = "REGULAR"
-  backup_retention_period_in_days                = 1
+  backup_retention_period_in_days                = "1"
   character_set                                  = "AL32UTF8"
   compartment_id                                 = var.compartment_ocid
-  compute_count                                  = 2
+  compute_count                                  = "2"
   compute_model                                  = "ECPU"
-  data_storage_size_in_tbs                       = 1
+  data_storage_size_in_tbs                       = "1"
   db_name                                        = var.adb_name
   db_version                                     = "26ai"
   db_workload                                    = "DW"
   display_name                                   = var.adb_name
-  is_auto_scaling_enabled                        = false
-  is_auto_scaling_for_storage_enabled            = false
-  is_dedicated                                   = false
-  is_mtls_connection_required                    = true
-  is_preview_version_with_service_terms_accepted = false
+  is_auto_scaling_enabled                        = "false"
+  is_auto_scaling_for_storage_enabled            = "false"
+  is_dedicated                                   = "false"
+  is_mtls_connection_required                    = "false"
+  is_preview_version_with_service_terms_accepted = "false"
   license_model                                  = var.license_model
   ncharacter_set                                 = "AL16UTF16"
+  subnet_id                                      = var.subnet_private_id 
 }
 
-# ウォレットをダウンロード
 resource "oci_database_autonomous_database_wallet" "generated_autonomous_data_warehouse_wallet" {
   autonomous_database_id = oci_database_autonomous_database.generated_database_autonomous_database.id
-  password               = var.adb_password
+  password               = var.db_password
   base64_encode_content  = "true"
   generate_type          = "SINGLE"
 }
